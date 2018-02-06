17f6971aee4a0fbd92dfc87779bb3266ff54080b
Wil Cook <wil.cook.is@gmail.com>
2018-02-06 12:14:04 -0500

    Bio text added, z-indexing removed, logos created.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,22 @@ import SnakeyA from '../components/SVGs/SnakeyA.js'
 import SnakeyB from '../components/SVGs/SnakeyB.js'
 import SnakeyC from '../components/SVGs/SnakeyC.js'
 
-const colors = ["#e7e8d4", "#003333", "#8f9a5e", "#ffff94", "#4D8C57", "#00755E", "#C9A0DC", "#AFE313", "" ]
+const colors = ["#e7e8d4", "#003333", "#8f9a5e", "#ffff94", "#4D8C57", "#00755E", "#C9A0DC", "#AFE313", "#B33B24", "#FD0E35", "#00CC99", "#01786F", "#76D7EA", ]
 const snakeColorA = colors[Math.floor(Math.random() * colors.length)]
 const snakeColorB = colors[Math.floor(Math.random() * colors.length)]
 const snakeColorC = colors[Math.floor(Math.random() * colors.length)]
 
 const IndexPage = () => (
   <div>
-   <div  className="mainBio">
-    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-   </div>
-    <div id="firstSnek">
-      <SnakeyB color={ snakeColorB }/>
-      <SnakeyA color={ snakeColorA }/>
-      <SnakeyC color={ snakeColorC }/>
-    </div>
-    <div id="nextSnek">
-      <SnakeyA color={ snakeColorB }/>
-      <SnakeyC color={ snakeColorA }/>
-      <SnakeyB color={ snakeColorC }/>
+    <div  className="mainBio">
+    <SnakeyB color={ snakeColorB }/>
+    <SnakeyA color={ snakeColorA }/>
+    <SnakeyC color={ snakeColorC }/>
+    Wiljago is pronounced "wil-ZHA-goh," but the nickname Wil is a lot easier to say.
+     Wil is a professional web designer and developer available for hire. He primarily works in Node.js, and especially loves React and React-Native.
+     <a target="blank" href="http://linkedin.com/in/wilcook">
+     </a>
+
     </div>
   </div>
 )
