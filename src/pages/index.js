f9ecbfb3beb0d463a72bd9723a5867449e9ee895
Wil Cook <wil.cook.is@gmail.com>
2018-02-07 09:36:20 -0500

    Front page complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,11 @@ import linkedin from '../images/linkedin.png';
 import github from '../images/github.png';
 import stackoverflow from '../images/stackoverflow.png';
 
+// external link addresses
+const GitHubURL = "https://github.com/wilcook";
+const StackOverflowURL = "https://stackoverflow.com/users/6845618/wiljago";
+const LinkedInURL = "http://linkedin.com/in/wilcook";
+
 // snake plant color array
 const colors = ["#e7e8d4", "#003333", "#8f9a5e", "#ffff94", "#4D8C57", "#00755E", "#C9A0DC", "#AFE313", "#B33B24", "#FD0E35", "#00CC99", "#01786F", "#76D7EA", "#805533"];
 
@@ -21,21 +26,30 @@ const snakeColorC = colors[Math.floor(Math.random() * colors.length)]
 
 const IndexPage = () => (
   <div>
-    <div  className="mainBio">
+  <div className="snek">
     <SnakeyB color={ snakeColorB }/>
     <SnakeyA color={ snakeColorA }/>
     <SnakeyC color={ snakeColorC }/>
+  </div>
+    <div  className="mainBio">
     <p>
      Wil Cook is a professional web designer and developer available for hire. He primarily works in Node.js, and especially loves React and React-Native.
+
      </p>
      <div className="logos">
+      <a href={ GitHubURL }>
       <img src={ github } height="30px" alt="GitHub" />
+      </a>
      </div>
      <div className="logos">
+     <a href={ StackOverflowURL }>
       <img src={ stackoverflow } height="30px" alt="GitHub" />
+      </a>
      </div>
      <div className="logos">
+     <a href={ LinkedInURL }>
       <img src={ linkedin } height="30px" alt="LinkedIn" />
+      </a>
      </div>
     </div>
   </div>
